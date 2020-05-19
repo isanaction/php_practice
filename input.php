02a60f7ca3ffdcc0ee54e2cc7f9b417245d3dab2
古藤勇魚 <>
2020-05-20 00:31:59 +0900

    GET POSTの違いを学習

--- input.php
+++ input.php
@@ -1,6 +1,7 @@
 <?php
-
-var_dump($_GET['name'])
+echo '<pre>';
+var_dump($_POST);
+echo '</pre>';
 
 ?>
 
@@ -14,9 +15,12 @@ var_dump($_GET['name'])
 </head>
 
 <body>
-    <form action="input.php" method="GET">
+    <form action="input.php" method="POST">
         名前
         <input type="text" name="name">
+        <input type="checkbox" name='sports[]' value="野球">野球
+        <input type="checkbox" name='sports[]' value="サッカー">サッカー
+        <input type="checkbox" name='sports[]' value="バスケ">バスケ
         <input type="submit" value="送信">
     </form>
 
