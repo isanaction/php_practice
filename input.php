00d9e3094bcedac91975273be6ea8c608d747ca9
古藤勇魚 <>
2020-05-19 21:56:18 +0900

    first commit

---
+++ input.php
@@ -0,0 +1,25 @@
+<?php
+
+var_dump($_GET['name'])
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="input.php" method="GET">
+        名前
+        <input type="text" name="name">
+        <input type="submit" value="送信">
+    </form>
+
+</body>
+
+</html>
